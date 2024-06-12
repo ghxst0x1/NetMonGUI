45a845f867882c75c5b02c13a24bb80ab997d924
ghxst0x1 <karthikrofficial7@gmail.com>
2024-06-12 18:56:20 +0100

    Lints and refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui::{Align, CentralPanel, Context, Layout, ScrollArea, SidePanel};
+use eframe::egui;
 use std::os::windows::process::CommandExt;
-use std::process::{Command, Stdio};
-use std::str;
-use winapi::um::winbase::CREATE_NO_WINDOW;
 
 #[derive(Default)]
 struct MyApp {
@@ -12,68 +9,69 @@ struct MyApp {
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self { output } = self;
-        SidePanel::left("side_panel").show(ctx, |ui| {
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
             ui.heading("Commands");
 
             if ui.button("START").clicked() {
-                match run_commands() {
-                    Ok(result) => *output = result,
-                    Err(e) => *output = format!("Error: {}", e),
-                }
+                *output = run_commands();
             }
-
             ui.vertical(|ui| {
-                ui.with_layout(Layout::bottom_up(Align::Center), |ui| {
+                ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
                     if ui.button("QUIT").clicked() {
-                        std::process::exit(0);
+                        ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
                     }
                     ui.separator();
                 });
             });
         });
-
-        CentralPanel::default().show(ctx, |ui| {
-            ScrollArea::vertical().auto_shrink(false).show(ui, |ui| {
-                ui.label(output.clone());
-            });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ScrollArea::vertical()
+                .auto_shrink(false)
+                .show(ui, |ui| {
+                    ui.label(output.clone());
+                });
         });
     }
 }
 
-fn run_commands() -> Result<String, Box<dyn std::error::Error>> {
-    let netstat_output = run_command("netstat", &["-ano"])?;
-    let lines: Vec<&str> = netstat_output.lines().collect();
-    let mut result = String::new();
-    for line in lines {
-        if line.contains("ESTABLISHED") {
-            if let Some(pid) = line.split_whitespace().last() {
-                let tasklist_output =
-                    run_command("tasklist", &["/svc", "/FI", &format!("PID eq {}", pid)])?;
-                result.push_str(&tasklist_output);
-            }
-        }
-    }
-    Ok(result)
+fn run_commands() -> String {
+    let netstat_output = run_command("netstat", &["-ano"]);
+    let result: Vec<String> = netstat_output
+        .lines()
+        .filter(|line| line.contains("ESTABLISHED"))
+        .flat_map(|line| {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            let pid = parts.last().unwrap();
+            run_command("tasklist", &["/svc", "/FI", &format!("PID eq {}", pid)])
+                .lines()
+                .map(String::from)
+                .collect::<Vec<String>>()
+        })
+        .collect();
+    result.join("\n")
 }
 
-fn run_command(command: &str, args: &[&str]) -> Result<String, Box<dyn std::error::Error>> {
+fn run_command(command: &str, args: &[&str]) -> String {
+    use std::process::{Command, Stdio};
+
     let output = Command::new(command)
         .args(args)
         .stdout(Stdio::piped())
-        .creation_flags(CREATE_NO_WINDOW)
-        .output()?;
-
-    Ok(String::from_utf8_lossy(&output.stdout).to_string())
+        .creation_flags(winapi::um::winbase::CREATE_NO_WINDOW)
+        .output()
+        .map(|output| output.stdout)
+        .map_err(|err| err.to_string())
+        .unwrap_or_default();
+    String::from_utf8_lossy(&output).to_string()
 }
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
+    let _ = eframe::run_native(
         "NetMonGUI",
         native_options,
         Box::new(|_cc| Box::<MyApp>::default()),
-    )
-    .expect("Failed to start eframe");
+    );
 }
